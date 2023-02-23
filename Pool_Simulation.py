124383815aae8913e6bd418b74cffe7b738ea35f
Jollyjep <Dcole875@gmail,com>
2023-02-23 15:32:50 +0000

    Defined primitive pool shapes in terms of 2d sets of numpy points for easier use as boundary conditions later

--- Pool_Simulation.py
+++ Pool_Simulation.py
@@ -1,12 +1,14 @@
+import numpy as np
+
 class Pool_Simulation:
-    def __init__(self, shape="circular", x_size=1.0, y_size=1.0, depth=1.0, viscosity=0.01, density=1000):
+    def __init__(self, shape="circular", x_size=1.0, y_size=1.0, depth=1.0, viscosity=0.01, density=1000, precision=10000):
         self.shape = shape
         self.x_size = x_size
         self.y_size = y_size
         self.depth = depth
         self.viscosity = viscosity
         self.density = density
-
+        self.pool_precision = precision
         #self.initial_conditions = initial_conditions
 
     def __str__(self):
@@ -18,8 +20,14 @@ class Pool_Simulation:
         primitives = ["circular", "rectangle"]
         if self.shape in primitives:
             if self.shape == "circular":
-                self.boundary = ["x**2+y**2 = {0}**2".format(self.x_size)]
+                theta = np.linspace(0, 2*np.pi, self.pool_precision)
+                self.boundary = self.circle_to_numpy(theta)
             if self.shape == "rectangle":
-                self.boundary = ["x=-{0}".format(self.x_size), "x={0}".format(self.x_size), "y=-{0}".format(self.y_size), "y={0}".format(self.y_size)]
+                self.boundary = np.array([np.array([-self.x_size, -self.x_size, self.x_size, self.x_size, -self.x_size]), np.array([-self.y_size, self.y_size, self.y_size, -self.y_size, -self.y_size])])
         else:
             print("Arbritary pools not yet implemented")
+
+    def circle_to_numpy(self, theta):
+        x = self.x_size*np.cos(theta)
+        y = self.y_size*np.sin(theta)
+        return np.array([x, y])
--- Surface_Waves_Core.py
+++ Surface_Waves_Core.py
@@ -1,13 +1,16 @@
 import numpy as np
+import scipy
 import numba
 from numba import njit
 from numba import cuda
-import scipy
+
 import pandas as pd
 from matplotlib import pyplot as plt
 import Pool_Simulation as Pool_sim
 
-def Pool_Simulation_Setup(shape="circular", x_dim=1, y_dim=1, z_dim=1, viscosity=1, density=1):
-    Pool_sim.Pool_Simulation(shape=shape, x_size=x_dim, y_size=y_dim, depth=z_dim, viscosity=viscosity, density=density)
+def Pool_Simulation_Setup(shape="rectangle", x_dim=1, y_dim=1, z_dim=1, viscosity=1, density=1):
+    Pool_boundaries = Pool_sim.Pool_Simulation(shape=shape, x_size=x_dim, y_size=y_dim, depth=z_dim, viscosity=viscosity, density=density)
+    Pool_boundaries.pool_boundary_creator()
+    Pool_boundaries.boundary
 
-
+Pool_Simulation_Setup()
