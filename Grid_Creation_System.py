f011c963526ba14145d11a48829cc9de4d2150e1
Jollyjep <Dcole875@gmail,com>
2023-02-27 16:46:50 +0000

    Updated numba code to fix depreciation warning

--- Grid_Creation_System.py
+++ Grid_Creation_System.py
@@ -1,7 +1,9 @@
 import numpy as np
 import numba
+from numba import jit
 from numba import njit
 from numba import cuda
+from numba.typed import List
 import scipy
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -11,33 +13,42 @@ import PIL
 
 class grid_creation:
 
-    def __init__(self, max_grid_size_x=1000, max_grid_size_y=1000):
-        self.grid_size_x = max_grid_size_x
-        self.grid_size_y = max_grid_size_y
+    def __init__(self, x_size, y_size):
+        self.x_scale = x_size
+        self.y_scale = y_size
 
     def grid_for_shape(self, pool):
         shape = pool.boundary
         img = shape.convert("RGBA")
         width, height = img.size
         pixels = np.asarray(img)
-        grid_x = [1]
-        grid_y = [1]
-        reflect = [True]
-        self.grid, self.reflect = self.quick_pixel(width, height, pixels, grid_x, grid_y, reflect)
-        print(self.grid)
+        grid_x = List()
+        grid_x.append(1)
+        grid_y = List()
+        grid_y.append(1)
+        reflect = List()
+        reflect.append(1)
+        grid_x, grid_y, reflect = self.quick_pixel(width, height, pixels, grid_x, grid_y, reflect)
+        grid_x = np.array(grid_x)
+        grid_y = np.array(grid_y)
+        grid = np.array([grid_x,grid_y])
+        divisor = np.array([np.full(np.shape(grid)[1], 1 / width), np.full(np.shape(grid)[1], 1 / height)])
+        self.grid = grid * divisor
+        self.grid_base = grid
+        self.reflect = np.array(reflect)
 
 
     @staticmethod
-    @njit(parallel=True)
+    @njit()
     def quick_pixel(width, height, pixels, grid_x, grid_y, reflect):
         for x in range(width):
             for y in range(height):
                 if pixels[x, y][0] < 5 and pixels[x, y][3] == 255:
                     grid_x.append(x)
                     grid_y.append(y)
-                    reflect.append(True)
-                elif pixels[x, y][0] >= 5 and pixels[x, y][3] == 255:
+                    reflect.append(1)
+                elif pixels[x, y][0] >= 5 and pixels[x, y][3] == 255 and pixels[x, y][1] == 0:
                     grid_x.append(x)
                     grid_y.append(y)
-                    reflect.append(False)
-        return np.array([grid_x[1:], grid_y[1:]]), np.array(reflect)
+                    reflect.append(0)
+        return grid_x[1:], grid_y[1:], reflect[1:]
--- Surface_Waves_Core.py
+++ Surface_Waves_Core.py
@@ -11,13 +11,14 @@ import Grid_Creation_System as gcs
 def Pool_Simulation_Setup(shape="circular", x_dim=1, y_dim=1, z_dim=1, viscosity=1, density=1):
     Pool_boundaries = Pool_sim.Pool_Simulation(shape=shape, x_size=x_dim, y_size=y_dim, depth=z_dim, viscosity=viscosity, density=density)
     Pool_boundaries.pool_boundary_creator()
-    #plt.plot(Pool_boundaries.boundary[0], Pool_boundaries.boundary[1])
     return Pool_boundaries
 
 
-def grid_setup(Pool_boundaries, x_grid=10, y_grid=10):
-    grid = gcs.grid_creation(x_grid, y_grid)
+def grid_setup(Pool_boundaries):
+    grid = gcs.grid_creation(Pool_boundaries.x_size, Pool_boundaries.y_size)
     grid.grid_for_shape(Pool_boundaries)
+    plt.scatter(grid.grid[0], grid.grid[1], s=2)
+    plt.show()
 
 
 Pool_boundaries = Pool_Simulation_Setup()
